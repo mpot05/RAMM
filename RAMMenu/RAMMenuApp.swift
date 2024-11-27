1921e99f3a7a50021d054ba4b83d5b449e1a2a87
Michael Potter <mpotter0905@gmail.com>
2024-11-27 10:42:52 -0500

    Init

--- RAMMenu/RAMMenuApp.swift
+++ RAMMenu/RAMMenuApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct RAMMenuApp: App {
+    @State var currentNumber: String = "1"
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
+            Button("One") {
+                currentNumber = "1"
+            }
+            Button("Two") {
+                currentNumber = "2"
+            }
+            Button("Three") {
+                currentNumber = "3"
+            }
+            Divider()
+            Button("Quit") {
+                NSApplication.shared.terminate(self)
+            }.keyboardShortcut("q")
         }
     }
 }

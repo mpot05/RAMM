4852a1cd99b64a7f6638067f4bcc0dfa881c3943
Michael Potter <mpotter0905@gmail.com>
2024-11-27 10:19:25 -0500

    Initial Commit

---
+++ RAMMenu/RAMMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAMMenuApp.swift
+//  RAMMenu
+//
+//  Created by Michael Potter on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct RAMMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RAMMenuTests/RAMMenuTests.swift
@@ -0,0 +1,17 @@
+//
+//  RAMMenuTests.swift
+//  RAMMenuTests
+//
+//  Created by Michael Potter on 11/27/24.
+//
+
+import Testing
+@testable import RAMMenu
+
+struct RAMMenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

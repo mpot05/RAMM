2ad408c878ac4e3e0eb7666b799633e11796a562
Michael Potter <mpotter0905@gmail.com>
2024-11-30 13:03:13 -0500

    no clue how to math the math

--- RAMMenu/RAMMenuApp.swift
+++ RAMMenu/RAMMenuApp.swift
@@ -12,11 +12,19 @@ import Darwin
 struct RAMMenuApp: App {
     @State var currentNumber: String = "1"
     @State var processInfo = ProcessInfo.processInfo
+    @State var ramUsage: String = ""
     var body: some Scene {
         MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
             Text("Total Ram: \(processInfo.physicalMemory/1024/1024/1000)GB")
-            Text("Ram Usage: \(getRamUsage())GB")
+            Text("Ram Usage: \(ramUsage)GB")
             Divider()
+                .onAppear {
+                    ramUsage = getRamUsage()
+                    Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (t) in
+                        ramUsage = getRamUsage()
+                        print("time")
+                    }
+                }
             Button("Quit") {
                 NSApplication.shared.terminate(self)
             }.keyboardShortcut("q")
@@ -42,7 +50,7 @@ struct RAMMenuApp: App {
         do {
             let regex = try NSRegularExpression(pattern: "(?:(?:\\b *)*([1234567890]+).)")
             let matches = regex.matches(in: outputString, range: NSRange(location: 0, length: outputString.count))
-            var result: [[String]] = []
+            var result: [String] = []
             for match in matches {
                 var groups: [String] = []
                 for rangeIndex in 1 ..< match.numberOfRanges {
@@ -52,20 +60,21 @@ struct RAMMenuApp: App {
                     groups.append(string)
                 }
                 if !groups.isEmpty {
-                    result.append(groups)
+                    result.append(groups[0])
                 }
             }
             print (result)
-            finalStrings.append(result[1][0])
-            finalStrings.append(result[3][0])
+            finalStrings.append(result[1])
+            finalStrings.append(result[3])
+            finalStrings.append(result[1])
         } catch {
             print("error \(error)")
         }
-        let num1 = ((Int(finalStrings[0]) ?? -1)*16384)
-        let num2 = ((Int(finalStrings[1]) ?? -1)*16384)
+        let num1 = ((Double(finalStrings[0]) ?? -1)*16384)
+        let num2 = ((Double(finalStrings[1]) ?? -1)*16384)
         let num3 = num1 + num2
         let num4 = processInfo.physicalMemory
-        let num5 = (((num4 - UInt64(num3))/1024)/1024)/1000
+        let num5 = (((Double(num4) - Double(num3))/1024)/1024)/1000
         
         
         return String(num5)
